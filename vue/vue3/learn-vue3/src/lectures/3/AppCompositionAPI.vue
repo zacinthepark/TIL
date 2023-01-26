2945e3fcac47d3b49792acd9ae826e6dee21d930
Jinwoo Park <zacinthepark@gmail.com>
2023-01-26 10:46:25 +0900

    compare options and composition api

---
+++ vue/vue3/learn-vue3/src/lectures/3/AppCompositionAPI.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+export default {
+	setup() {
+		const counter = ref(0);
+
+		const increment = () => counter.value++;
+
+		onMounted(() => {
+			console.log('Component Has Mounted!');
+		});
+
+		return {
+			counter,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue/vue3/learn-vue3/src/lectures/3/AppOptionsAPI.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			counter: 0,
+		};
+	},
+	methods: {
+		increment() {
+			this.counter++;
+		},
+	},
+	mounted() {
+		console.log('Component Has Mounted!');
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
