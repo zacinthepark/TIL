d7f899fbfcfdbcd417ebd0fba12fd48dd5b8d341
Jinwoo Park <zacinthepark@gmail.com>
2023-01-26 16:21:17 +0900

    class and style binding

---
+++ vue/vue3/learn-vue3/src/lectures/9/App_style_binding.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+			perferendis, praesentium ea eveniet, at accusantium architecto dolor
+			deleniti cum natus assumenda magni error molestias officia. Blanditiis vel
+			beatae voluptatum repudiandae.
+		</div>
+		<button v-on:click="fontSize++">+</button>
+		<button v-on:click="fontSize--">-</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return {
+			styleObject,
+			fontSize,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
