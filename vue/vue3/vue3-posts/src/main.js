f210513eea55179f71bc082a5f7f53a36f3f5c5f
Jinwoo Park <zacinthepark@gmail.com>
2023-01-30 15:27:26 +0900

    vue router basics

--- vue/vue3/vue3-posts/src/main.js
+++ vue/vue3/vue3-posts/src/main.js
@@ -2,5 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-createApp(App).mount('#app');
+import router from '@/router';
+
+createApp(App).use(router).mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
