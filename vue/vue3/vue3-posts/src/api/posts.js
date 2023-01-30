49b87fa3c815f8ba1eac5b0765a65bab1254e75d
zacinthepark <zacinthepark@gmail.com>
2023-01-30 23:33:07 +0900

    paging and filter at postlistview

--- vue/vue3/vue3-posts/src/api/posts.js
+++ vue/vue3/vue3-posts/src/api/posts.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
-// axios
-const posts = [
-	{ id: 1, title: '제목1', content: '내용1', createdAt: '2023-01-30' },
-	{ id: 2, title: '제목2', content: '내용2', createdAt: '2023-02-30' },
-	{ id: 3, title: '제목3', content: '내용3', createdAt: '2023-03-30' },
-	{ id: 4, title: '제목4', content: '내용4', createdAt: '2023-04-30' },
-	{ id: 5, title: '제목5', content: '내용5', createdAt: '2023-05-30' },
-];
 
-export function getPosts() {
-	return axios.get('http://localhost:3000/posts');
+export function getPosts(params) {
+	return axios.get('http://localhost:3000/posts', { params });
 }
 
 export function getPostById(id) {
