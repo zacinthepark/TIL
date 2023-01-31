6807cffce4afe464ef9eba0bd65586f5665eb001
Jinwoo Park <zacinthepark@gmail.com>
2023-01-31 17:55:14 +0900

    edit alert button

---
+++ vue/vue3/vue3-posts/src/components/AppAlert.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="app-alert alert alert-success" role="alert">
+		수정이 완료되었습니다!
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.app-alert {
+	position: fixed;
+	top: 10px;
+	right: 10px;
+}
+</style>
--- vue/vue3/vue3-posts/src/views/posts/PostEditView.vue
+++ vue/vue3/vue3-posts/src/views/posts/PostEditView.vue
@@ -18,6 +18,7 @@
 				<button class="btn btn-primary">수정</button>
 			</template>
 		</PostForm>
+		<AppAlert></AppAlert>
 	</div>
 </template>
 
@@ -26,6 +27,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '../../api/posts';
 import PostForm from '../../components/posts/PostForm.vue';
+import AppAlert from '../../components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
