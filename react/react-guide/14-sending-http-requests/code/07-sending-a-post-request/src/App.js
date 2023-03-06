4b8f0d705259969606ca9efd0af7200348dfaa87
zacinthepark <zacinthepark@gmail.com>
2023-03-06 23:21:06 +0900

    practice post request with firebase

--- react/react-guide/14-sending-http-requests/code/07-sending-a-post-request/src/App.js
+++ react/react-guide/14-sending-http-requests/code/07-sending-a-post-request/src/App.js
@@ -13,7 +13,8 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
+      // const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
+      const response = await fetch('https://react-http-aa6ec-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -43,7 +44,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json', {
+    const response = await fetch('https://react-http-aa6ec-default-rtdb.firebaseio.com/movies.json', {
       method: 'POST',
       body: JSON.stringify(movie),
       headers: {
