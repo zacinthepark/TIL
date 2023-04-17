6de7f434fc46a68164fed603860cc62429e5507f
zacinthepark <zacinthepark@gmail.com>
2023-04-18 00:32:27 +0900

    configuring typescript

---
+++ typescript/ConfiguringTypeScript/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2
+};
+const juniper = {
+    breed: 'Silkie',
+    eggsPerWeek: 5,
+    name: 'Juniper',
+    age: 3
+};
---
+++ typescript/ConfiguringTypeScript/src/index.ts
@@ -0,0 +1,21 @@
+interface Chicken {
+  breed: string,
+  eggsPerWeek: number,
+  name: string
+  age: number
+}
+
+const norma: Chicken = {
+  breed: 'Silkie',
+  eggsPerWeek: 4,
+  name: 'Norma',
+  age: 2
+}
+
+const juniper: Chicken = {
+  breed: 'Silkie',
+  eggsPerWeek: 5,
+  name: 'Juniper',
+  age: 3
+
+}
