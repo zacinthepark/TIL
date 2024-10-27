6b210bbc8335a4d42a3e7101c0b538ed951c97a9
zacinthepark <zacinthepark@gmail.com>
2024-10-28 02:45:50 +0900

    java start

---
+++ langs/java/java-start-dev/src/HelloJava.java
@@ -0,0 +1,7 @@
+// 파일 이름과 클래스 이름을 같아야한다.
+public class HelloJava {
+    // main() 메서드는 파일의 시작 지점이다.
+    public static void main(String[] args) {
+        System.out.println("hello java");  // 세미콜론으로 문장을 구분한다.
+    }
+}
